6bac1bb2d794fb78c53d4a2490f89b260dfd0c64
jeevesh <jeevesh2802@gmail.com>
2026-03-07 20:14:24 +0530

    added the test file

---
+++ test/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        assert Math.sqrt(25) == 5;
+        assert Math.pow(2, 3) == 8;
+        assert Math.log(Math.exp(1)) == 1;
+
+    }
+}
